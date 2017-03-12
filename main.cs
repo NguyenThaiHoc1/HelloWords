01dabeef4b95ef5fa056e9abc1cd8651e7b408b6
NguyenThaiHoc1 <thaihocmap123@gmail.com>
2017-03-12 22:28:03 +0700

    Angela: Add class ClassMate

--- main.cs
+++ main.cs
@@ -1,4 +1,10 @@
 using System;
+public class CLassMate {
+	asdasdasdhashdasdaskjd
+sadhasdhaskjhdkasjdhaskjdd
+djaskjdajkshdakjhdjkasdasjkdhajsdsad
+sadhasjdhasdhaskdhaskjhdjashdaksd
+}
 public class Text {
 	public string name = "Beckham Angela";	
 	public static void main () {
